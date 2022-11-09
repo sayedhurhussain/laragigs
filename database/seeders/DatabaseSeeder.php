0179013892f7c39ed52cc94d71bf92c9348e1230
sayedhurhussain <sayedhurhussain55@gmail.com>
2022-11-09 15:33:00 +0500

    add data using seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +15,40 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(5)->create();
+
+        Listing::factory(6)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
+        // ]);
+
+        // Listing::create([
+        //     'title' => 'Laravel Senior Developer',
+        //     'tags' => 'laravel, javascript',
+        //     'company' => 'Acme Crop',
+        //     'location' => 'boston MA',
+        //     'email' => 'email11@gmail.com',
+        //     'website' => 'http://www.acme.com',
+        //     'description' => '
+        //     Laravel programmers must have proficient knowledge of fundamental web 
+        //     technologies. They must have a thorough understanding of CSS, HTML, and 
+        //     JavaScript to be able to build and deploy the applications. Prepare a list 
+        //     of interview questions for Laravel developers, including these technologies.'
+        // ]);
+
+        // Listing::create([
+        //     'title' => 'Full-Stack Engineer',
+        //     'tags' => 'laravel, backend, api',
+        //     'company' => 'Stark Industries',
+        //     'location' => 'New York, NY',
+        //     'email' => 'email12@gmail.com',
+        //     'website' => 'http://www.starkindustries.com',
+        //     'description' => 'A full-stack engineer is a high-level software engineer 
+        //     that works to design, test, and implement various software applications. 
+        //     They create software, applications, and scalable web services, while also 
+        //     providing leadership for coding teams.'
         // ]);
     }
 }
