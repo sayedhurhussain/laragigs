97f36418749bb21d8013bce6410ef08a347c1035
sayedhurhussain <sayedhurhussain55@gmail.com>
2022-11-10 10:46:17 +0500

    get routes view update

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -13,7 +13,7 @@
 ><i class="fa-solid fa-arrow-left"></i> Back
 </a>
 <div class="mx-4">
-<div class="bg-gray-50 border border-gray-200 p-10 rounded">
+<x-card class="p-10 "> 
     <div
         class="flex flex-col items-center justify-center text-center"
     >
@@ -25,28 +25,9 @@
 
         <h3 class="text-2xl mb-2">{{$listing->title}}</h3>
         <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
-        <ul class="flex">
-            <li
-                class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-            >
-                <a href="#">Laravel</a>
-            </li>
-            <li
-                class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-            >
-                <a href="#">API</a>
-            </li>
-            <li
-                class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-            >
-                <a href="#">Backend</a>
-            </li>
-            <li
-                class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-            >
-                <a href="#">Vue</a>
-            </li>
-        </ul>
+
+        <x-listing-tags :tagsCsv="$listing->tags"/>
+
         <div class="text-lg my-4">
             <i class="fa-solid fa-location-dot"></i>{{$listing->location}}
         </div>
@@ -74,6 +55,6 @@
             </div>
         </div>
     </div>
-</div>
+</x-card>
 </div>
 @endsection
--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -29,51 +29,7 @@
     <p>
         {{$listing['description']}}
     </p> --}}
-
-    <div class="bg-gray-50 border border-gray-200 rounded p-6">
-        <div class="flex">
-            <img
-                class="hidden w-48 mr-6 md:block"
-                src="{{asset('images/no-image.png')}}"
-                alt=""
-            />
-            <div>
-                <h3 class="text-2xl">
-                    {{-- 1st method to declear title --}}
-                    {{-- <a href="show.html">{{$listing['title']}}</a> --}}
-                    {{-- 2nd method to declear title --}}
-                    <a href="/listings/{{$listing->id}}">{{$listing['title']}}</a>
-
-                </h3>
-                <div class="text-xl font-bold mb-4">Acme Corp</div>
-                <ul class="flex">
-                    <li
-                        class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                    >
-                        <a href="#">Laravel</a>
-                    </li>
-                    <li
-                        class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                    >
-                        <a href="#">API</a>
-                    </li>
-                    <li
-                        class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                    >
-                        <a href="#">Backend</a>
-                    </li>
-                    <li
-                        class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                    >
-                        <a href="#">Vue</a>
-                    </li>
-                </ul>
-                <div class="text-lg mt-4">
-                    <i class="fa-solid fa-location-dot"></i>{{$listing->location}}
-                </div>
-            </div>
-        </div>
-    </div>
+    <x-listing-card :listing="$listing" />
 @endforeach
 
 @else
